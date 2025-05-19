8f9d8bbaa51fdf3f9e6b16b62bbd10257059c985
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-05-18 21:14:08 -0300

    feat: add sitemap.xml and robots.txt for SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://regina-portfolio-rn5x8i0x2-regina-pompeos-projects.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
